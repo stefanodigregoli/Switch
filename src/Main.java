b1671c8c2d5df1cf52dfce85371745167bd8c873
stefanodigregoli <stefano.digregoli2001@gmail.com>
2023-06-16 12:11:29 +0200

    Switch Statement Loop infinite times asking the user to input a number between 1 and 10 Print the name of the number (e.g. 5 --> five) If the user input a number lower than 1 or greater than 10, print Cannot give you the name

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int number = 10;
+        String numberName;
+        switch (number){
+
+        case 1:
+            numberName = "Uno";
+            System.out.println("il numero è" + numberName);
+            break;
+
+        case 2:
+            numberName = "Due";
+            System.out.println("il numero è" + numberName);
+            break;
+
+        case 3:
+            numberName = "Tre";
+            System.out.println("il numero è" + numberName);
+            break;
+
+        case 4:
+            numberName = "Quattro";
+            System.out.println("il numero è" + numberName);
+            break;
+
+        case 5:
+            numberName = "Cinque";
+            System.out.println("il numero è" + numberName);
+            break;
+
+        case 6:
+            numberName = "Sei";
+            System.out.println("il numero è" + numberName);
+            break;
+
+        case 7:
+            numberName = "Sette";
+            System.out.println("il numero è" + numberName);
+            break;
+
+        case 8:
+            numberName = "Otto";
+            System.out.println("il numero è" + numberName);
+            break;
+
+        case 9:
+            numberName = "Nove";
+            System.out.println("il numero è" + numberName);
+            break;
+
+            case 10:
+        numberName = " Dieci ";
+        System.out.println("il numero è" + numberName);
+        break;
+        default:
+            System.out.println("error");
+
+
+        }
+    }
+    }
